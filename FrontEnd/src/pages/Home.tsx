5491315c2053ebb537013561e9a2d2e09e16aefe
andrearg <andrearivassg@gmail.com>
2025-03-26 07:59:07 -0700

    Setting color palette and font on tailwind

---
+++ FrontEnd/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div
+      className="flex flex-col items-center justify-center h-screen bg-darkBackground ">
+      <h1 className="text-4xl font-bold font-primary text-darkBgText">Bienvenido a Atenea</h1>
+      <p className="text-lg mt-4 text-darkSecondary font-primary">Tu plataforma de estudio inteligente.</p>
+    </div>
+  );
+};
+
+export default Home;
