0bbf0ca3145cf65c04f5e033e7111241dc5aae6e
andrearg <andrearivassg@gmail.com>
2025-05-28 02:27:28 -0700

    Vercel: Fixing vite config

--- FrontEnd/src/services/analyticsService.ts
+++ FrontEnd/src/services/analyticsService.ts
@@ -16,7 +16,7 @@ import {
 import { API_CONFIG } from '../config';
 
 
-const API_BASE_URL = process.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export class AnalyticsService {
   private static getAuthHeaders() {
